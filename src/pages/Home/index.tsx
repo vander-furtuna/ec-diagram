ff2c0f3ce94d3d67a3b53b5d026ca46981d07857
Vanderlei Furtuna <furtunavanderlei@gmail.com>
2024-10-08 22:16:51 -0300

    fix: scroll and foote

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,18 @@
+import { useCallback, useEffect } from 'react';
 import { Dashboard } from '../../containers/Dashboard';
 import { Informations } from '../../containers/Informations';
 import { HomeContainer } from './styles';
 
 export function Home() {
+
+    const redirectToExternalSite = useCallback((url: string) => {
+        window.location.href = url;
+    }, []);
+
+    useEffect(() => {
+        redirectToExternalSite('https://ufc-flow.vercel.app/');
+    }, [redirectToExternalSite]);
+
     return (
         <HomeContainer>
             <Dashboard />
